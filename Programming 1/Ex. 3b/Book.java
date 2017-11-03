804a66dd1ea8925c22b0a448a6decc87ff541756
Julius O <33136533+jo16v@users.noreply.github.com>
2017-11-03 07:37:21 +0100

    Update Book.java

--- Programming 1/Ex. 3b/Book.java
+++ Programming 1/Ex. 3b/Book.java
@@ -1,4 +1,5 @@
-// Done By: Julius Oeftiger 16-127-532; Brian Schweigler 16-102-071
+// Done By: Julius O. 16-xxx-yyy; 
+// Brian S. 16-xxx-yyy
 /* ************************************************************************* *\
 *                Programmierung 1 HS 2017 - Serie 2-2                         *
 \* ************************************************************************* */
@@ -9,87 +10,85 @@ import java.text.*;
 
 public class Book {
 
-  private int id;
-  private String title;
-  private String author;
-  private Date dateOfPublication;
+private int id;
+  	private String title;
+	private String author;
+ 	private Date dateOfPublication;
 	private int price;
 
-  public static final String DATE_FORMAT = "dd.MM.yyyy";
+  	public static final String DATE_FORMAT = "dd.MM.yyyy";
 
-
-  public Book(int id, String title, String author, Date dateOfPublication, int price) {          //constructor
-    this.id = id;
-    this.title = title;
-    this.author = author;
-    this.dateOfPublication = dateOfPublication;
+	//constructor
+  	public Book(int id, String title, String author, Date dateOfPublication, int price) {
+    	this.id = id;
+    	this.title = title;
+    	this.author = author;
+    	this.dateOfPublication = dateOfPublication;
 		this.price = price;
-  }
+  	}
 
-  /** Returns the age of the book in days since publication */
-  public int age() {
-
-    Date dateNow = new Date();
-    long bookAge = ( dateNow.getTime() - dateOfPublication.getTime() ) / (24*60*60*1000);   // Calculate age of Book with method datenow
+  	/** Returns the age of the book in days since publication */
+  	public int age() {
+    	Date dateNow = new Date();
+    	long bookAge = ( dateNow.getTime() - dateOfPublication.getTime() ) / (24*60*60*1000);
 
     return (int) bookAge; //return the age of the book in days
-  }
+  	}
 
-  /** Returns a String representation of the book */
-  public String toString() {
+ 	/** Returns a String representation of the book */
+  	public String toString() {
+    	return id +", "+ title +", "+ author +", "+ dateToString(dateOfPublication) +", "+ age() +" days old";        //return book details as string
+  	}
 
-    return id +", "+ title +", "+ author +", "+ dateToString(dateOfPublication) +", "+ age() +" days old";        //return book details as string
-  }
+  	/** Reads all book data from user input */
+  	public void input() {
 
-  /** Reads all book data from user input */
-  public void input() {
+    	Scanner scn1 = new Scanner( System.in );  // seperate scanner because of weird bug when combining scn.nextInt(); with scn.nextLine();
+    	System.out.print( "Please enter id: " );	
+    	this.id = scn1.nextInt();
 
-    Scanner scn1 = new Scanner( System.in );  // seperate scanner because of weird bug when combining scn.nextInt(); with scn.nextLine();
-    System.out.print( "Please enter id: " );
-    this.id = scn1.nextInt();
-
-    Scanner scn = new Scanner( System.in );
-    System.out.print( "\nPlease enter title: " );
-    this.title = scn.nextLine();
+    	Scanner scn = new Scanner( System.in );
+    	System.out.print( "\nPlease enter title: " );
+    	this.title = scn.nextLine();
 
 
-    System.out.print( "\nPlease enter author: " );
-    this.author = scn.nextLine();
+    	System.out.print( "\nPlease enter author: " );
+    	this.author = scn.nextLine();
 
-    System.out.print( "\nPlease enter the date of publication (dd.MM.YYYY): " );
-    this.dateOfPublication = stringToDate( scn.nextLine() );
-    scn1.close();
-    scn.close();
-  }
+    	System.out.print( "\nPlease enter the date of publication (dd.MM.YYYY): " );
+    	this.dateOfPublication = stringToDate( scn.nextLine() );
+    	scn1.close();
+    	scn.close();
+  	}
 
-  // get-set
-  public int getID() {
-    return id;
-  }
-  public void setID(int id) {
-    this.id = id;
-  }
+  	// get-set
+  	public int getID() {
+    	return id;
+  	}
+  	public void setID(int id) {
+    	this.id = id;
+  	}
 
-  public String getTitle() {
-    return title;
-  }
-  public void setTitle(String title) {
-    this.title = title;
-  }
+  	public String getTitle() {
+    	return title;
+  	}
+  	public void setTitle(String title) {
+    	this.title = title;
+  	}
 
-  public String getAuthor() {
-    return author;
-  }
-  public void setAuthor(String author) {
-    this.author = author;
-  }
+  	public String getAuthor() {
+    	return author;
+  	}
+  	public void setAuthor(String author) {
+    	this.author = author;
+  	}
 
-  public Date getDateOfPublication() {
-    return dateOfPublication;
-  }
-  public void setDateOfPublication(Date dateOfPublication) {
-    this.dateOfPublication = dateOfPublication;
-  }
+  	public Date getDateOfPublication() {
+    	return dateOfPublication;
+  	}
+  	public void setDateOfPublication(Date dateOfPublication) {
+    	this.dateOfPublication = dateOfPublication;
+  	}
 	public int setPrice() {
 		this.price = price;
 	}
@@ -99,25 +98,25 @@ public class Book {
 		this.price = 0
 	}
 
-  //--- helper methods -- DO NOT CHANGE ------------------------------------
-  /** Converts the Date object d into a String object */
-  public static String dateToString( Date d ) {
+  	//--- helper methods -- DO NOT CHANGE ------------------------------------
+  	/** Converts the Date object d into a String object */
+  	public static String dateToString( Date d ) {
 
-    SimpleDateFormat fmt = new SimpleDateFormat( DATE_FORMAT );
-    return fmt.format( d );
-  }
+    	SimpleDateFormat fmt = new SimpleDateFormat( DATE_FORMAT );
+    	return fmt.format( d );
+  	}
 
-  /** Converts the String object s into a Date object */
-  public static Date stringToDate( String s ) {
+  	/** Converts the String object s into a Date object */
+  	public static Date stringToDate( String s ) {
 
-    Date r = null;
-    try {
-      SimpleDateFormat fmt = new SimpleDateFormat( DATE_FORMAT );
-      r = fmt.parse( s );
-    } catch ( ParseException e ) {
-      System.err.println( e );
-      System.exit(1);
-    }
-    return r;
-  }
+    	Date r = null;
+    	try {
+      		SimpleDateFormat fmt = new SimpleDateFormat( DATE_FORMAT );
+      		r = fmt.parse( s );
+    	} catch ( ParseException e ) {
+      		System.err.println( e );
+      		System.exit(1);
+    	}
+    	return r;
+  	}
 }
